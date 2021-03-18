eadff213a4d82c1ef44c45be1898bc10f52f01f8
Arthur Correnson <arthur.correnson@gmail.com>
2021-03-18 22:22:45 +0100

    Refactor the why3 code & update the makefile

--- Makefile
+++ Makefile
@@ -1,3 +1,9 @@
 
 src/proof.ml: theories/proof.why
 	why3 extract -D ocaml64 -L ./theories --recursive-deps $< -o $@
+
+.PHONY: prove
+prove: ./theories/*.why
+	why3 prove -P Alt-Ergo -L ./theories $^
+
+
--- src/proof.ml
+++ src/proof.ml
@@ -1,24 +1,24 @@
-type l0 =
+type prop =
   | Atom of int
-  | And of l0 * l0
-  | Or of l0 * l0
-  | Impl of l0 * l0
+  | And of prop * prop
+  | Or of prop * prop
+  | Impl of prop * prop
   | Bot
 
-type proof_tree =
-  | Pr_axiom of l0
-  | Pr_Iimpl of proof_tree * l0
-  | Pr_Eimpl of proof_tree * proof_tree * l0
-  | Pr_Ior1 of proof_tree * l0
-  | Pr_Ior2 of proof_tree * l0
-  | Pr_Eor of proof_tree * proof_tree * proof_tree * l0
-  | Pr_Eand1 of proof_tree * l0
-  | Pr_Eand2 of proof_tree * l0
-  | Pr_Iand of proof_tree * proof_tree * l0
-  | Pr_RAA of proof_tree * l0
-  | Pr_Ebot of proof_tree * l0
+type proof =
+  | Pr_axiom of prop
+  | Pr_Iimpl of proof * prop
+  | Pr_Eimpl of proof * proof * prop
+  | Pr_Ior1 of proof * prop
+  | Pr_Ior2 of proof * prop
+  | Pr_Eor of proof * proof * proof * prop
+  | Pr_Eand1 of proof * prop
+  | Pr_Eand2 of proof * prop
+  | Pr_Iand of proof * proof * prop
+  | Pr_RAA of proof * prop
+  | Pr_Ebot of proof * prop
 
-let conseq (pr: proof_tree) : l0 =
+let conseq (pr: proof) : prop =
   match pr with
   | Pr_axiom p -> p
   | Pr_Iimpl (_, p) -> p
@@ -32,38 +32,46 @@ let conseq (pr: proof_tree) : l0 =
   | Pr_RAA (_, p) -> p
   | Pr_Ebot (_, p) -> p
 
-let rec eqb (x: l0) (y: l0) : bool =
+let rec infix_eqeq (x: prop) (y: prop) : bool =
   match (x, y) with
   | (Atom n, Atom m) -> n = m
-  | (And (a, b), And (a', b')) -> eqb a a' && eqb b b'
-  | (Or (a, b), Or (a', b')) -> eqb a a' && eqb b b'
-  | (Impl (a, b), Impl (a', b')) -> eqb a a' && eqb b b'
+  | (And (a, b), And (a', b')) -> infix_eqeq a a' && infix_eqeq b b'
+  | (Or (a, b), Or (a', b')) -> infix_eqeq a a' && infix_eqeq b b'
+  | (Impl (a, b), Impl (a', b')) -> infix_eqeq a a' && infix_eqeq b b'
   | (Bot, Bot) -> true
   | (_, _) -> false
 
-let rec contain (x: l0) (g: l0 list) : bool =
+let rec contain (x: prop) (g: prop list) : bool =
   match g with
   | [] -> false
-  | y :: ys -> eqb y x || contain x ys
+  | y :: ys -> infix_eqeq y x || contain x ys
 
-let rec check_proof (g: l0 list) (pr: proof_tree) : bool =
+let rec check_proof (g: prop list) (pr: proof) : bool =
   match pr with
   | Pr_axiom p -> contain p g
   | Pr_Iimpl (prq,
               Impl (p,
                     q)) ->
-    eqb (conseq prq) q && check_proof (p :: g) prq
+    infix_eqeq (conseq prq) q && check_proof (p :: g) prq
   | Pr_Eimpl (pr1,
               pr2,
               p) ->
     begin match conseq pr1 with
       | Impl (a,
               b) ->
-        eqb (conseq pr2) a && eqb p b && check_proof g pr1 && check_proof g pr2
+        infix_eqeq (conseq pr2) a && infix_eqeq p b && check_proof g pr1 && 
+        check_proof g
+          pr2
       | _ -> false
     end
-  | Pr_Ior1 (pr1, Or (a, _)) -> eqb (conseq pr1) a && check_proof g pr1
-  | Pr_Ior2 (pr1, Or (_, b)) -> eqb (conseq pr1) b && check_proof g pr1
+  | Pr_Ior1 (pr1,
+             Or (a,
+                 _)) ->
+    infix_eqeq (conseq pr1) a && check_proof g pr1
+  | Pr_Ior2 (pr1,
+             Or (_,
+                 b)) ->
+    infix_eqeq (conseq pr1) b && check_proof g pr1
   | Pr_Eor (pru,
             pra,
             prb,
@@ -75,8 +83,10 @@ let rec check_proof (g: l0 list) (pr: proof_tree) : bool =
                cx),
          Impl (y,
                cy)) ->
-        eqb a x && eqb b y && eqb cx q && eqb cy q && check_proof g pru && 
-        check_proof g pra && 
+        infix_eqeq a x && infix_eqeq b y && infix_eqeq cx q && infix_eqeq cy q && 
+        check_proof g
+          pru && check_proof g
+          pra && 
         check_proof g
           prb
       | _ -> false
@@ -84,26 +94,27 @@ let rec check_proof (g: l0 list) (pr: proof_tree) : bool =
   | Pr_Eand1 (pr1,
               p) ->
     begin match conseq pr1 with
-      | And (a, _) -> eqb a p && check_proof g pr1
+      | And (a, _) -> infix_eqeq a p && check_proof g pr1
       | _ -> false
     end
   | Pr_Eand2 (pr1,
               p) ->
     begin match conseq pr1 with
-      | And (_, a) -> eqb a p && check_proof g pr1
+      | And (_, a) -> infix_eqeq a p && check_proof g pr1
       | _ -> false
     end
   | Pr_Iand (pra,
              prb,
              And (a,
                   b)) ->
-    eqb (conseq pra) a && eqb (conseq prb) b && check_proof g pra && 
+    infix_eqeq (conseq pra) a && infix_eqeq (conseq prb) b && check_proof g
+      pra && 
     check_proof g
       prb
   | Pr_RAA (pra,
             p) ->
     begin match conseq pra with
-      | Impl (Impl (a, Bot), Bot) -> eqb a p && check_proof g pra
+      | Impl (Impl (a, Bot), Bot) -> infix_eqeq a p && check_proof g pra
       | _ -> false
     end
   | Pr_Ebot (pr1,
